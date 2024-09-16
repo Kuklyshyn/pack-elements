cbeedb4943e7c918e530dfbdd68f5080403d27c4
Mykola Kuklyshyn <kuklishinnick@gmail.com>
2024-09-16 10:46:59 +0200

    push

--- src/panels.ts
+++ src/panels.ts
@@ -4,15 +4,10 @@ import { cmdClear, cmdDeviceDesktop, cmdDeviceMobile, cmdDeviceTablet } from './
 
 export default (editor: Editor, opts: Required<PluginOptions>) => {
   const { Panels } = editor;
-  const { cmdOpenImport, cmdTglImages } = opts;
-  const openExport = 'export-template';
   const openStyleManager = 'open-sm';
   const openTraits = 'open-tm';
   const openLayers = 'open-layers';
   const openBlocks = 'open-blocks';
-  const activateOutline = 'sw-visibility';
-  const activateFullscreen = 'fullscreen';
-  const activatePreview = 'preview';
   const iconStyle = 'style="display: block; max-width: 22px"';
 
   // Turn off default devices select and create new one
@@ -49,11 +44,6 @@ export default (editor: Editor, opts: Required<PluginOptions>) => {
     {
       id: 'options',
       buttons: [{
-        id: activatePreview,
-        context: activatePreview,
-        command: activatePreview,
-        label: `<svg ${iconStyle} viewBox="0 0 24 24"><path fill="currentColor" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z"></path></svg>`
-      }, {
         id: 'undo',
         command: 'core:undo',
         label: `<svg ${iconStyle} viewBox="0 0 24 24">
