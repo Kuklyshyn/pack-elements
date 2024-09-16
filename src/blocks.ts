c2c8f332dd0289bb7a44f678c683f324e9c1ad97
Mykola Kuklyshyn <kuklishinnick@gmail.com>
2024-09-16 12:24:39 +0200

    add style

--- src/blocks.ts
+++ src/blocks.ts
@@ -89,7 +89,17 @@ export default function (editor: Editor, opts: Required<PluginOptions>) {
     media: `<svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M20 20.5C20 21.3 19.3 22 18.5 22H13C12.6 22 12.3 21.9 12 21.6L8 17.4L8.7 16.6C8.9 16.4 9.2 16.3 9.5 16.3H9.7L12 18V9C12 8.4 12.4 8 13 8S14 8.4 14 9V13.5L15.2 13.6L19.1 15.8C19.6 16 20 16.6 20 17.1V20.5M20 2H4C2.9 2 2 2.9 2 4V12C2 13.1 2.9 14 4 14H8V12H4V4H20V12H18V14H20C21.1 14 22 13.1 22 12V4C22 2.9 21.1 2 20 2Z" />
     </svg>`,
-    content: '<a class="button">Button 777</a>',
+    content: `<a class="button" style="
+      background-color: #000000;
+      border-radius: 0 0px 0px 0px;
+      color: white;
+      font-family: Helvetica, sans-serif;
+      font-size: 14px;
+      margin: "20px auto 25px auto",
+      padding: 10px 15px 10px 15px;
+      display: inline-block;
+      text-decoration: none;
+    ">Button 11111</a>`,
   });
 
   addBlock("divider", {
@@ -167,7 +177,16 @@ export default function (editor: Editor, opts: Required<PluginOptions>) {
     content: {
       type: "link",
       content: "Link",
-      style: { color: "#3b97e3" },
+      style: {
+        color: "white",
+        display: "block",
+        backgroundColor: "#000000",
+        borderRadius: "0 0px 0px 0px",
+        fontFamily: "Helvetica, sans-serif",
+        fontSize: "14px",
+        margin: "20px auto 25px auto",
+        padding: "10px 15px 10px 15px",
+      },
     },
   });
 
